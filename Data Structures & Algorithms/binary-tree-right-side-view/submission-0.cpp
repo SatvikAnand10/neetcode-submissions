a7211792b2ddad916b6098d4addcb88acec8dc4a
SatvikAnand10 <135153653+SatvikAnand10@users.noreply.github.com>
2026-08-23 19:44:54 +0530

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> ans;
+        if(!root) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> a;
+            for(int i=0;i<size;i++){                
+                TreeNode* temp=q.front();
+                q.pop();
+                a.push_back(temp->val);
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+            ans.push_back(a);
+        }
+        return ans;
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        vector<vector<int>> x=levelOrder(root);
+        for(auto a:x){
+            ans.push_back(a.back());
+        }
+        return ans;
+    }
+};
