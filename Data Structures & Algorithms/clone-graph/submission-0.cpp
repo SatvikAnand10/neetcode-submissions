9888df4088b711f1d21a7cba22ea08d45d19b582
SatvikAnand10 <135153653+SatvikAnand10@users.noreply.github.com>
2026-08-30 14:02:36 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*,Node*> mp;
+
+    Node* cloneGraph(Node* node) {
+        return node?clone(node):nullptr;
+    }
+
+    Node* clone(auto node){
+        if(mp.find(node)!=mp.end()){
+            return mp[node];
+        }
+        Node* copy=new Node(node->val);
+        mp[node]=copy;
+        for(auto a:node->neighbors){
+            copy->neighbors.push_back(clone(a));
+        }
+        return copy;
+    }
+};
